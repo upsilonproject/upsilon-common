057f67762ea3114d7f24a64115cbaa61fb5bc9a8
jamesread <jread@redhat.com>
2013-05-30 21:24:18 +0100

    Helper script for db.

---
+++ var/importdb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Everyone likes a nice big fat warning message...
+echo "------------------------------------------------------------------------"
+echo "WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING"
+echo ""
+echo ">>> THIS SCRIPT WILL ***DESTROY*** YOUR DATABASE."
+echo ""
+echo "WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING"
+echo "------------------------------------------------------------------------"
+echo " "
+echo "This script is indented for developers. "
+echo " "
+echo "------------------------------------------------------------------------"
+echo "Press CTRL+C to quit without importing anything."
+read -sp "Enter your MySQL password (ENTER for none) to continue: " $sqlpassword
+
+if [ -n "$sqlpassword" ]; then
+		sqlpassword="";
+else
+		sqlpassword="-p $sqlpassword";
+fi
+
+mysql -u root $sqlpassword -e 'CREATE DATABASE upsilon'
+mysql -u root $sqlpassword upsilon < schema.sql 
+mysql -u root $sqlpassword upsilon < initialData.sql
