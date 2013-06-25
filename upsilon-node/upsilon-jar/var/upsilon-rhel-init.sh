4508eb29c8d51dc7fe590cb499949ec6122f835f
jread <jread@redhat.com>
2013-06-25 13:14:43 +0100

    Added specific RPMs for Fedora and RHEL.

---
+++ upsilon-node/upsilon-jar/var/upsilon-rhel-init.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+#
+# upsilon        Startup script for upsilon.
+#
+# chkconfig: 2345 12 88
+# description: Upsilon is a distributed, flexible and extensible \
+# system monitoring application
+### BEGIN INIT INFO
+# Provides: $upsilon
+# Required-Start: $local_fs $network
+# Required-Stop: $local_fs 
+# Default-Start:  2 3 4 5
+# Default-Stop: 0 1 6
+# Description: Upsilon is a distributed, flexible and extensible \
+#		system monitoring application
+### END INIT INFO
+
+# Source function library.
+. /etc/init.d/functions
+
+RETVAL=0
+PID=`ps aux | grep upsilon.jar | grep -v grep | awk '{print $2}'`
+
+prog=upsilon
+exec="/usr/bin/java -Djava.net.preferIPv4Stack=true -jar /usr/share/upsilon/upsilon.jar"
+
+start() {
+	if [[ -z "$PID" ]]; then
+		echo -n $"Upsilon is starting... "
+		nohup $exec 2>/dev/null 1> /dev/null &
+		RETVAL=$?
+
+		echo
+
+		if [[ 0 -eq $RETVAL ]]; then
+			echo "OK!";
+		else
+			echo "Failed :("
+		fi
+
+		return $RETVAL
+	else
+		echo "Upsilon is already running, wont start it again."
+	fi
+}
+
+stop() {
+	if [[ -z "$PID" ]]; then
+		echo "Upsilon is not running."
+	else
+		PID=`ps aux | grep upsilon.jar | grep -v grep | awk '{print $2}'`
+
+		kill -9 $PID
+		echo -n $"Stopped upsilon ($PID). "
+
+		RETVAL=$?
+		echo
+
+		return $RETVAL
+	fi
+}
+
+status() {
+	if [[ -z "$PID" ]]; then
+		echo "Upsilon does not appear to be running."
+	else
+		echo "Upsilon is running as PID: $PID"
+	fi
+}
+
+restart() {
+        stop
+        start
+}
+
+case "$1" in
+  start)
+        start
+        ;;
+  stop)
+        stop
+        ;;
+  restart)
+        restart
+        ;;
+  reload)
+        exit 3
+        ;;
+  force-reload)
+        restart
+        ;;
+  status)
+	status
+        ;;
+  *)
+        echo $"Usage: $0 {start|stop|restart|status}"
+        exit 3
+esac
+
+exit $?
