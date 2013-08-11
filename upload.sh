1567fb70d4d84ad469fc85ce7c3fc99666642834
jamesread <jread@redhat.com>
2013-08-11 23:27:35 +0100

    Added rpm's and deb's to upload.sh.

--- upload.sh
+++ upload.sh
@@ -1,15 +1,17 @@
-basepath="upsilon-project.co.uk:public_html/releases/"
+basepath="/public_html/releases"
+
+VERSION_NODE=0.114.0
 
 srcs=()
-files+=("upsilon-mobileWeb/bin/foo.apk apk/");
+files+=("upsilon-mobileWeb/bin/upsilon-mobileWeb.apk apk/");
 files+=("upsilon-web/upsilon-web-tgz/target/upsilon-web-1.2.0.tar.gz upsilon-web-tgz/");
+files+=("upsilon-node/upsilon-rpm-generic/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-generic.noarch.rpm rpm-generic/");
+files+=("upsilon-node/upsilon-deb/target/upsilon-node-${VERSION_NODE}.deb deb/");
 
 for file in "${files[@]}"; do
 	src=`echo "$file" | awk '{print $1}'`
 	dst=`echo "$file" | awk '{print $2}'`
 
-	echo $src;
-	echo $dst;
 	if [ ! -f "$src" ]; then
 		echo "Build does not look complete, missing file."
 		echo "Filename: $src" 
@@ -18,11 +20,13 @@ for file in "${files[@]}"; do
 done
 
 echo "Will upload ${#files[@]} file(s)."
-
+echo
 
 for file in "${files[@]}"; do
 	src=`echo "$file" | awk '{print $1}'`
 	dst=`echo "$file" | awk '{print $2}'`
 
-	echo "put $file" | sftp "$basepath/$dst" 
+	echo "File: $src -> $basepath/$dst"
+	echo "put $src" | sftp -b - upsilon-project.co.uk:$basepath/$dst
+	echo "-"
 done
