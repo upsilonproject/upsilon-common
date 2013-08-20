da81e468d2b62d3f8dda1d07868768090a8f158d
jread <jread@redhat.com>
2013-08-20 13:29:03 +0100

    Dojo built from mvn script.

--- upsilon-web/buildDojo.sh
+++ upsilon-web/buildDojo.sh
@@ -1,6 +1,7 @@
 # Note: edit src/main/dojo/profile.js
 rm -rf target
-mkdir target
+mkdir -p target/dojo-upsilon/
+echo "buildDojo.sh pwd: `pwd`"
 ./var/dojo-release/util/buildscripts/build.sh profile=src/main/dojo/profile.js
 #cp target/dojo-upsilon/dojo/dojo.js src/main/php/resources/dojo/dojo/dojo.js
 cp -r target/dojo-upsilon/* src/main/php/resources/dojo/
