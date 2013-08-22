9f1fccf883b6c2c6bcec879d2fd48af3be562f71
jamesread <jread@redhat.com>
2013-08-23 00:49:55 +0100

    Improved icon markup. Added default icon.

--- upsilon-web/src/main/php/resources/images/serviceIcons/makeSprites.sh
+++ upsilon-web/src/main/php/resources/images/serviceIcons/makeSprites.sh
@@ -6,12 +6,13 @@ rm sprites.html
 
 echo '<link rel = "stylesheet" type = "text/css" href = "sprites.css" />' >> sprites.html
 echo '<style type = "text/css">div { text-align: center; margin: .5em; overflow: hidden; display: inline-block; position: relative; width: 200px; height: 70px; border: 1px solid black;}</style>' >> sprites.html
+echo ".serviceIcon { display: inline-block; min-width: 16px; min-height: 16px; background-image: url('sprites.png'); background-repeat: no-repeat } " >> sprites.css
 
 for file in *.png; do
 	name=`echo $file | sed 's/\.png//g'`
 
-	echo "img.$name { background-image: url('sprites.png'); background-position: 0px -${x}px; background-repeat: no-repeat;}" >> sprites.css
-	echo "<div>$file <br /><img width = "16" height = "16" class = '$name' /></div>" >> sprites.html
+	echo "span.$name { background-position: 0px -${x}px; }" >> sprites.css
+	echo "<div>$file <br /><span title = '$name' class = 'serviceIcon $name'></span></div>" >> sprites.html
 
 	let x+=16
 done
