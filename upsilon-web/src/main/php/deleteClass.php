b3f46541fbb54083985071bf98ebfb8320b09e7f
jamesread <contact@jwread.com>
2013-08-23 23:40:41 +0100

    Added some pretty icons. Few basic things around about classes.

---
+++ upsilon-web/src/main/php/deleteClass.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'includes/common.php';
+
+$sql = 'DELETE FROM classes WHERE id = :id';
+$stmt = stmt($sql);
+$stmt->bindValue(':id', san()->filterUint('id'));
+$stmt->execute();
+
+redirect('listClasses.php');
+
+
