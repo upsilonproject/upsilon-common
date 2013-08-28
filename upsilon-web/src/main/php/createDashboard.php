3c28302d00a07055d9aaa8bfc5914d873ce9fa1d
root <root@upsilon.teratan.net>
2013-08-28 10:51:54 +0100

    Typo in createDashboard.php

--- upsilon-web/src/main/php/createDashboard.php
+++ upsilon-web/src/main/php/createDashboard.php
@@ -2,7 +2,7 @@
 
 require_once 'includes/common.php';
 
-$sql = 'INSERT INTO dashboard (title) values ("Dasboard")';
+$sql = 'INSERT INTO dashboard (title) values ("Dashboard")';
 $stmt = stmt($sql)->execute();
 
 redirect('viewDashboard.php?id=' . db()->lastInsertId());
