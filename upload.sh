9ef658c8fb7af5e9b755de52e0801f1bd3db6a19
jread <jread@redhat.com>
2013-09-05 10:44:36 +0100

    Added a zip output for the web project.

--- upload.sh
+++ upload.sh
@@ -6,7 +6,10 @@ basepath="/public_html/"
 
 srcs=()
 files+=("upsilon-mobileWeb/bin/upsilon-mobileWeb.apk releases/apk/");
+
 files+=("upsilon-web/upsilon-web-tgz/target/upsilon-web-${VERSION_WEB}.tar.gz releases/upsilon-web-tgz/");
+files+=("upsilon-web/upsilon-web-tgz/target/upsilon-web-${VERSION_WEB}.zip releases/upsilon-web-tgz/");
+
 files+=("upsilon-node/upsilon-rpm-fedora18/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-1.fc18.noarch.rpm releases/rpm-fedora18/");
 files+=("upsilon-node/upsilon-rpm-fedora18/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-1.fc18.noarch.rpm repositories/rpm-fedora18/");
 
