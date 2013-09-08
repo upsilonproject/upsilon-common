745113ff80c8bfee87d89f68249901081df2b20e
jamesread <jread@redhat.com>
2013-09-08 15:26:49 +0100

    Continued cleaning packages. logging.xml now shipped in packages, upsilon references changed to upsilon-node. etc etc.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
-
-VERSION_NODE=0.116.0
+VERSION_NODE=0.117.0
 VERSION_WEB=1.3.0
 
 basepath="/public_html/"
@@ -10,18 +9,18 @@ srcs=()
 #files+=("upsilon-mobileWeb/bin/upsilon-mobileWeb.apk releases/apk/");
 
 # upsilon-web
-files+=("upsilon-web/upsilon-web-tgz/target/upsilon-web-${VERSION_WEB}.tar.gz releases/upsilon-web-tgz/");
-files+=("upsilon-web/upsilon-web-tgz/target/upsilon-web-${VERSION_WEB}.zip releases/upsilon-web-tgz/");
+#files+=("upsilon-web/upsilon-web-tgz/target/upsilon-web-${VERSION_WEB}.tar.gz releases/upsilon-web-tgz/");
+#files+=("upsilon-web/upsilon-web-tgz/target/upsilon-web-${VERSION_WEB}.zip releases/upsilon-web-tgz/");
 
 # upsilon-node
-files+=("upsilon-mobileWeb/bin/upsilon-mobileWeb.apk releases/apk/");
-
-files+=("upsilon-node/upsilon-rpm-fedora18/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-1.fc18.noarch.rpm releases/rpm-fedora18/");
-files+=("upsilon-node/upsilon-rpm-generic/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-1.generic.noarch.rpm releases/rpm-generic/");
-files+=("upsilon-node/upsilon-rpm-rhel6/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-1.el6.noarch.rpm releases/rpm-rhel6/");
+files+=("upsilon-node/upsilon-node-rpm-fedora18/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-1.fc18.noarch.rpm releases/rpm-fedora18/");
+files+=("upsilon-node/upsilon-node-rpm-generic/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-1.generic.noarch.rpm releases/rpm-generic/");
+files+=("upsilon-node/upsilon-node-rpm-rhel6/target/rpm/upsilon-node/RPMS/noarch/upsilon-node-${VERSION_NODE}-1.el6.noarch.rpm releases/rpm-rhel6/");
 
 files+=("upsilon-node/upsilon-node-deb/target/upsilon-node-${VERSION_NODE}.deb releases/deb/");
+
 files+=("upsilon-node/upsilon-node-tgz/target/upsilon-node-${VERSION_NODE}.tar.gz releases/tgz/");
+files+=("upsilon-node/upsilon-node-tgz/target/upsilon-node-${VERSION_NODE}.zip releases/tgz/");
 
 for file in "${files[@]}"; do
 	src=`echo "$file" | awk '{print $1}'`
