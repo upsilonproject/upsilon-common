333e77171df7ff97cbc019d16012c9bdac66575c
jamesread <jread@redhat.com>
2013-11-09 11:40:35 +0000

    Added upsilon-force-peer-update, moved error() into upsiloncommon

--- upsilon-node/upsilon-jar/var/tools/upsiloncommon.py
+++ upsilon-node/upsilon-jar/var/tools/upsiloncommon.py
@@ -3,6 +3,7 @@
 import httplib
 import sys
 import socket
+import argparse
 
 def getHttpClient(ssl, address, port):
 	if ssl:
@@ -25,11 +26,28 @@ def getHttpContent(client, url):
 		print "If this server is running SSL, try again with --ssl"
 		sys.exit()
 
-	assert res.status == 200, "Expected HTTP 200."
+	if res.status != 200:
+		error("Requested: %s, Expected HTTP 200, got HTTP %d" % (url, res.status))
 
 	res = res.read()
 
 	assert len(res) > 0, "Expected non-empty response."
 
 	return res
+
+def commonArgumentParser():
+	parser = argparse.ArgumentParser();
+	parser.add_argument('--address', '-a', help = "Hostname or IP address of upsilon-node", default = "localhost")
+	parser.add_argument('--port', '-p', help = "Port", default = 4000)
+	parser.add_argument('--ssl', action = "store_true")
+
+	return parser
+
+def error(message = None, e = None):
+	print "[ERROR]", message
+
+	if not e == None:
+		print "Exception:", str(e)
+
+	sys.exit(1)
 
