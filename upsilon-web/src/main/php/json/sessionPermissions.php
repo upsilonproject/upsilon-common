03aae8321a729931f384d6e6025521cda2a4ae9e
jamesread <contact@jwread.com>
2013-11-10 01:56:25 +0000

    Crude Login & Logout, node list view.

--- upsilon-web/src/main/php/json/sessionPermissions.php
+++ upsilon-web/src/main/php/json/sessionPermissions.php
@@ -7,6 +7,8 @@ use \libAllure\Session;
 outputJson(array(
 	'viewDashboard' => Session::hasPriv('VIEW_DASHBOARD'),
 	'viewServices' => Session::hasPriv('VIEW_SERVICES'),
+	'viewNodes' => Session::hasPriv('VIEW_NODES'),
+	'loggedIn' => Session::isLoggedIn(),
 ));
 
 ?>
