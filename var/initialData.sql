f7096b9b52b665dd473397dd3be2de94e4f81058
jread <jread@redhat.com>
2013-11-12 13:34:12 +0000

    The clock now no longer uses jQuery, and is included after initial installation.

--- var/initialData.sql
+++ var/initialData.sql
@@ -6,5 +6,6 @@ INSERT INTO widgets (`class`) VALUES ('Nodes');
 INSERT INTO widgets (`class`) VALUES ('Tasks');
 INSERT INTO widgets (`class`) VALUES ('Events');
 INSERT INTO widgets (`class`) VALUES ('ServicesFromGroup');
+INSERT INTO widgets (`class`) VALUES ('Clock');
 INSERT INTO classes (`title`, `l`, `r`) VALUES ('Root class', 0, 1);
 INSERT INTO dashboard (`title`) VALUES ('First Dashboard');
