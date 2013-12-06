a17bb649fcc0b43494f9b627da3be77c67d404e8
jamesread <contact@jwread.com>
2013-12-06 22:52:21 +0000

    Added support for "news"

---
+++ upsilon-web/src/main/php/json/getNews.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'jsonCommon.php';
+
+$group = san()->filterUint('group');
+$services = getServices($group);
+
+$news = array();
+$times = array();
+
+foreach ($services as $service) {
+	if (!isset($service['news'])) {
+		continue;
+	}
+
+	foreach ($service['news'] as $key => $story) {
+		$news[] = $story;
+		$times[$key] = $story['time'];
+	}
+}
+
+array_multisort($times, SORT_DESC, $news);
+
+$news = array_slice($news, 0, 10, true);
+
+outputJson($news);
+
+?>
